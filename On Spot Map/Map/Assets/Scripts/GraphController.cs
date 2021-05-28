1d1e0b962734a38563652bd76b9ffe8993365137
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-05-28 23:59:40 +0530

    Added On Spot Map section.

---
+++ On Spot Map/Map/Assets/Scripts/GraphController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GraphController : MonoBehaviour
+{
+    public float WaitTime;
+
+    [SerializeField]
+    API api;
+
+    [SerializeField]
+    TextMeshPro title;
+
+    void Start()
+    {
+        api.GetTimeData(onDataReceived);
+    }
+
+    void onDataReceived(List<TimeData> dataList)
+    {
+        StartCoroutine(CycleDataRoutine(dataList));
+    }
+
+    IEnumerator CycleDataRoutine(List<TimeData> dataList)
+    {
+        while (true)
+        {
+            foreach (TimeData data in dataList)
+            {
+                title.text = data.confirmed.ToString();
+                yield return new WaitForSeconds(WaitTime);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ On Spot Map/Map/Assets/Scripts/TimeData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeData : MonoBehaviour
+{
+    public string Region;
+    public long confirmed;
+    public int active;
+    public int cured;
+    public int d;
+}
